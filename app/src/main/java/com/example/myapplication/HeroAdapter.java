f581f17d56d06093acc4a3b698eba8d117e10875
12050 <1205095339@qq.com>
2023-03-09 10:25:20 +0800

    listView

--- app/src/main/java/com/example/myapplication/HeroAdapter.java
+++ app/src/main/java/com/example/myapplication/HeroAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,10 +25,23 @@ public class HeroAdapter extends ArrayAdapter<Item> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Item item = getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
-        Button button = (Button) view.findViewById(R.id.item_button);
-        TextView textView = (TextView) view.findViewById(R.id.item_text);
-        textView.setText(item.getText());
+        View view;
+        ViewHolder viewHolder;
+        if(convertView==null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.btn = view.findViewById(R.id.item_button);
+            viewHolder.title = view.findViewById(R.id.item_text);
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.title.setText(item.getText());
         return view;
+    }
+    public static class ViewHolder {
+        public Button btn;
+        public TextView title;
     }
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +27,15 @@ public class SecondActivity extends AppCompatActivity {
             actionBar.hide();
         }
         listView.setAdapter(heroAdapter);
+        listView.setFocusable(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Item item = itemList.get(i);
+                Toast.makeText(SecondActivity.this, item.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void initItems() {
